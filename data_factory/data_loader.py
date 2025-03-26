a1e96c02d6d5271eb482f619251e511431c85d21
infogroup502 <energysmartcontrol@126.com>
2025-03-26 17:14:41 +0800

    Update data_loader.py

--- data_factory/data_loader.py
+++ data_factory/data_loader.py
@@ -90,14 +90,55 @@ class PUMPSegLoader(object):
             return np.float32(self.test[
                               index // self.step * self.win_size:index // self.step * self.win_size + self.win_size]), np.float32(
                 self.test_labels[index // self.step * self.win_size:index // self.step * self.win_size + self.win_size])
+            
+class SKABSegLoader(object):
+    def __init__(self, data_path, win_size, step, mode="train"):
+        self.mode = mode
+        self.step = step
+        self.win_size = win_size
+        self.scaler = StandardScaler()
+        data = np.load(data_path + "/SKAB_train.npy")
+        data = np.nan_to_num(data)
+        self.scaler.fit(data)
+        data = self.scaler.transform(data)
+        test_data = np.load(data_path + "/SKAB_test.npy")
+        test_data = np.nan_to_num(test_data)
+        self.test = self.scaler.transform(test_data)
+        self.train = data
+        self.val = self.test
+        self.test_labels = np.load(data_path + "/SKAB_test_label.npy")
 
+    def __len__(self):
+        if self.mode == "train":
+            return (self.train.shape[0] - self.win_size) // self.step + 1
+        elif (self.mode == 'val'):
+            return (self.val.shape[0] - self.win_size) // self.step + 1
+        elif (self.mode == 'test'):
+            return (self.test.shape[0] - self.win_size) // self.step + 1
+        else:
+            return (self.test.shape[0] - self.win_size) // self.win_size + 1
+
+    def __getitem__(self, index):
+        index = index * self.step
+        if self.mode == "train":
+            return np.float32(self.train[index:index + self.win_size]), np.float32(self.test_labels[0:self.win_size])
+        elif (self.mode == 'val'):
+            return np.float32(self.val[index:index + self.win_size]), np.float32(self.test_labels[0:self.win_size])
+        elif (self.mode == 'test'):
+            return np.float32(self.test[index:index + self.win_size]), np.float32(
+                self.test_labels[index:index + self.win_size])
+        else:
+            return np.float32(self.test[
+                              index // self.step * self.win_size:index // self.step * self.win_size + self.win_size]), np.float32(
+                self.test_labels[index // self.step * self.win_size:index // self.step * self.win_size + self.win_size])
 
 def get_loader_segment(index, data_path, batch_size, win_size=100, step=100, mode='train', dataset='KDD'):
     if (dataset == 'Dodgers'):
         dataset = DodgersSegLoader(data_path, win_size, 1, mode)
     elif (dataset == 'PUMP'):
         dataset = PUMPSegLoader(data_path, win_size, 1, mode)
-
+    elif (dataset == 'SKAB'):
+        dataset = SKABSegLoader(data_path, win_size, 1, mode)
     shuffle = False
     if mode == 'train':
         shuffle = True
@@ -105,6 +146,6 @@ def get_loader_segment(index, data_path, batch_size, win_size=100, step=100, mod
     data_loader = DataLoader(dataset=dataset,
                              batch_size=batch_size,
                              shuffle=shuffle,
-                             num_workers=8,
+                             num_workers=0,
                              drop_last=False)
     return data_loader
